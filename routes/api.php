e849465f95971cd040c1dbbc08c2f444333b71b7
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2026-01-12 18:08:29 +0600

    solve extension auto improt

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/users', function () {
-    return new UserResource(User::all());
+Route::get('/users', function(){
+    return UserResource::collection(User::all());
 });
-
