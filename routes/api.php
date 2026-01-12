c63663f7df9dce80fc86f7d93d3032f1e3212302
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2026-01-12 17:42:17 +0600

    user get route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/users', function () {
+    return new UserResource(User::all());
 });
+
