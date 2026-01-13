f4adefb19f21f1f1e7b3aab6686440f0c916cf62
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2026-01-13 15:17:10 +0600

    migrate product table

--- database/migrations/2026_01_13_085743_create_products_table.php
+++ database/migrations/2026_01_13_085743_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("title", 80);
+            $table->longText("description")->nullable();
+            $table->string("image_url",220)->nullable();
+            $table->decimal("price", 8, 2);
+            $table->foreignId("user_id")->constrained('users');
             $table->timestamps();
         });
     }
