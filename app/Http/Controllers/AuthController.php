bbc50fe198dadfc3c30dedb1b379db65968578bf
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2026-01-13 17:13:54 +0600

    controller -> -> regiister -> login -> profile -> logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,29 +32,43 @@ class AuthController extends Controller
     }
 
     // login api
+    // public function Login(Request $request){
+    //     $fields = $request->validate([
+    //         'email' => 'required|string',
+    //         'password' => 'required|string'
+    //     ]);
+
+    //     // check email
+    //     $user = User::where('email', $fields['email'])->first();
+
+    //     // check password
+    //     if(!$user || !bcrypt($fields['password'], $user->password)){
+    //         return response([
+    //             'message' => 'Bad creds'
+    //         ], 401);
+    //     }
+
+    //     $token = $user->createToken('myapptoken')->plainTextToken;
+
+    //     $response = [
+    //         'user' => $user,
+    //         'token' => $token
+    //     ];
+    //     return response($response, 201);
+    // }
+
+    // login api 
     public function Login(Request $request){
-        $fields = $request->validate([
-            'email' => 'required|string',
-            'password' => 'required|string'
-        ]);
 
-        // check email
-        $user = User::where('email', $fields['email'])->first();
-
-        // check password
-        if(!$user || !bcrypt($fields['password'], $user->password)){
-            return response([
-                'message' => 'Bad creds'
-            ], 401);
-        }
-
-        $token = $user->createToken('myapptoken')->plainTextToken;
-
-        $response = [
-            'user' => $user,
-            'token' => $token
-        ];
-        return response($response, 201);
     }
 
+    // logout api
+    public function Logout(){
+
+    }
+
+    // profile api
+    public function Profile(){
+        
+    }
 };
